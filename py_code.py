0e6594d9e21eb4854dca2a3a12f5831a96fc54ec
JonatasDeOliveira <joc@cin.ufpe.br>
2018-11-22 08:42:59 -0300

    refactoring

--- py_code.py
+++ py_code.py
@@ -1,26 +1,55 @@
 import pandas as pd
 import os
+import sys
 from fuctions import *
         
-dbNSFP_DIRECTORY = "/bioinfo/dbNSFPv3.5a/"
-dbNSFP_FILENAME = "dbNSFP3.5a_variant.chrM"
+def main(file_dir, file_name, filter_dir, percent_dir):
 
-COLUMN_NAMES = ["genename", "rs_dbSNP150", "hg19_pos(1-based)", "cds_strand", "codonpos", "codon_degeneracy",
-                "ref", "alt", "aaref", "aaalt", "aapos", "Ensembl_geneid", "Ensembl_transcriptid",
-                "Ensembl_proteinid", "Uniprot_acc_Polyphen2", "Transcript_id_VEST3", "SIFT_pred",
-                "Polyphen2_HDIV_pred", "Polyphen2_HVAR_pred", "LRT_pred", "MutationTaster_pred",
-                "MutationAssessor_pred", "FATHMM_pred", "PROVEAN_pred", "VEST3_score", "MetaSVM_pred",
-                "MetaLR_pred", "M-CAP_pred", "REVEL_score", "MutPred_score", "CADD_phred", "DANN_score",
-                "fathmm-MKL_coding_pred"]
+	dbNSFP_DIRECTORY = file_dir
+	dbNSFP_FILENAME = file_name
+	filterRS_DIRECTORY = filter_dir
+	percent_DIRECTORY = percent_dir
 
-pd.set_option('display.max_columns', 300)
+	columns_to_filter = ["genename", "rs_dbSNP150", "hg19_pos(1-based)", "cds_strand", "codonpos", "codon_degeneracy",
+		        "ref", "alt", "aaref", "aaalt", "aapos", "Ensembl_geneid", "Ensembl_transcriptid",
+		        "Ensembl_proteinid", "Uniprot_acc_Polyphen2", "Transcript_id_VEST3", "SIFT_pred",
+		        "Polyphen2_HDIV_pred", "Polyphen2_HVAR_pred", "LRT_pred", "MutationTaster_pred",
+		        "MutationAssessor_pred", "FATHMM_pred", "PROVEAN_pred", "VEST3_score", "MetaSVM_pred",
+		        "MetaLR_pred", "M-CAP_pred", "REVEL_score", "MutPred_score", "CADD_phred", "DANN_score",
+		        "fathmm-MKL_coding_pred"]
 
-df = pd.read_csv(dbNSFP_DIRECTORY + dbNSFP_FILENAME, sep='\t')
+	pd.set_option('display.max_columns', 300)
+	
+	#Read the dataset from a csv file
+	df = pd.read_csv(dbNSFP_DIRECTORY + dbNSFP_FILENAME, sep='\t')
+	
 
-#df = df[COLUMN_NAMES]
+	#filter_columns_df = filter_by_columns(df, columns_to_filter)
 
-df = df.loc[df['rs_dbSNP150'] != '.']
+	cell_filter_df = filter_by_cell_value(df, 'rs_dbSNP150')
 
-info_df = parallelize_information_rate(df, 80, 48)
+	#info_df = get_df_percent(df, 80, 48)
 
-info_df.to_csv('Information Rate/' + dbNSFP_FILENAME + '.csv')
+
+	#info_df.to_csv(percent_DIRECTORY + dbNSFP_FILENAME + '.csv')
+	cell_filter_df.to_csv(filterRS_DIRECTORY + dbNSFP_FILENAME + '.csv')
+
+
+def filter_by_cell_value(df, cell_value):
+	df = df.loc[df[cell_value] != '.']
+	return df
+
+def filter_by_columns(df, columns):
+	df = df[columns]
+	return columns
+
+def get_df_percent(df, partition_num, cores_num):
+	info_df = parallelize_information_rate(df, partition_num, cores_num)
+	return info_df
+
+if __name__ == "__main__":
+	if len(sys.argv) < 5:
+		print("ERROR: Missing parameters")
+		print("USAGE: python3 py_code.py dbNSFP_DIRECTORY dbNSFP_FILENAME filterRS_DIRECTORY percent_DIRECTORY")
+	else:
+		main(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])
